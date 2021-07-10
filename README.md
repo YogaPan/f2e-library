a12b6581e300cbf71c08b0a507c3dbefc67495dd
Galtz <galtz0321@gmail.com>
2021-07-10 11:25:32 +0800

    feat: bootstrap project

---
+++ README.md
@@ -0,0 +1,3 @@
+# F2E Library
+
+TODO
---
+++ packages/browserslist-config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  production: [">0.25%", "last 2 version", "not dead", "not ie <= 11"],
+  development: [
+    "last 1 chrome version",
+    "last 1 firefox version",
+    "last 1 safari version",
+  ],
+};
